77a95327b5474b58968b0a6e0516e99f14e08443
yuliyago <yuuliya02@proton.me>
2023-06-25 14:14:38 +0300

    Work with multiple if cases has been done.

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if(year%400==0 && year %100==0) System.out.println("leap");
+        else if (year%4==0 && year %100>0) System.out.println("leap");
+        else System.out.println("not leap");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,29 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        String numberOfDays;
+        switch (month){
+            case (2):
+                numberOfDays="28";
+                break;
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                numberOfDays="31";
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                numberOfDays="30";
+                break;
+            default: numberOfDays="wrong number!";
+            break;
+        }
+        System.out.println(numberOfDays);
     }
 }
